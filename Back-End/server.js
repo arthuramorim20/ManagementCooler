441e0c4bebf55345a646f8c5de813c246c0b49a0
Arthur <aer47186@gmail.com>
2025-02-19 12:20:49 -0400

    Adicionando arquitetura de pastas

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import router from './src/routes/main.js'
 import cors from 'cors'
+import bodyParser from 'body-parser';
 const port = 8080;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(router);
+app.use(bodyParser)
+
 
 app.listen(port || 8080, (error) => {
     if (error) {
