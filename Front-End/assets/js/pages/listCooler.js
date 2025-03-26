e636157d8aeacec483038daa56279588703d8b83
Arthur <aer47186@gmail.com>
2025-03-26 16:15:09 -0400

    Voltando

--- Front-End/assets/js/pages/listCooler.js
+++ Front-End/assets/js/pages/listCooler.js
@@ -1,10 +1,17 @@
 const usersList = document.querySelector('.resultUsers');
+const bod = document.querySelector('.result')
 
 usersList.addEventListener('click', async e => {
     const el = e.target;
 
-    const response = await fetch('')
+    const response = await fetch('http://localhost:8080/users', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
     if (el.classList.contains('listar')) {
-        
+        const strJson = JSON.stringify(response)
+        bod.innerHTML = `${strJson}`;
     }
 })
