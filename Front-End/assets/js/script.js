b5c2e7204592d18c3675404a8eb0f17da5270153
Arthur <aer47186@gmail.com>
2025-04-08 09:53:48 -0400

    add script on all pages

---
+++ Front-End/assets/js/script.js
@@ -0,0 +1,25 @@
+const divSearch = document.querySelector('.search-div');
+const searchBtn = divSearch.querySelector('.btn');
+
+let behaviorBtnSearch = document.createElement('input');
+behaviorBtnSearch.className = searchBtn.className;
+behaviorBtnSearch.style.cssText = searchBtn.style.cssText;
+behaviorBtnSearch.style.textAlign = 'left'
+
+
+
+/*let elIcon = 
+`
+    <div class="search-input">
+        <input type="btn btn-search btn-outline-success visually-hidden placeholder="Pesquisar"></input> 
+    </div>
+`
+*/
+
+divSearch.addEventListener('click', e => {
+    e.preventDefault();
+    
+    if (e.target.classList.contains('btn')) {
+        searchBtn.replaceWith(behaviorBtnSearch) 
+    }
+})
