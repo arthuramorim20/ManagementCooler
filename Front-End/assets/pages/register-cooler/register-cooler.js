2f913ab8f1b0dbf977b1ad8700c223176f50cfaf
Arthur <aer47186@gmail.com>
2025-04-08 15:11:10 -0400

    ++`

---
+++ Front-End/assets/pages/register-cooler/register-cooler.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#main');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch('http://localhost:8080/new_user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            responsavel: responsavel.value,
+            setor: setor.value,
+            marca: marca.value,
+            capacidade: capacidade.value,
+            gas: gas.value,
+            servicos: servicos.value,
+            tecnico: tecnico.value,
+            proxmanutencao: proxmanutencao.value,
+            status: estado.value
+        })
+    })
+
+    console.log('Foi')
+})
