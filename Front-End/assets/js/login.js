f41199950d0556d3d62bd0a53c7db3d366eda68b
Arthur <aer47186@gmail.com>
2025-04-09 10:17:29 -0400

    editing

---
+++ Front-End/assets/js/login.js
@@ -0,0 +1,12 @@
+const btnSucess = document.querySelector('.alert-sucess');
+
+
+/**const alertError =
+`
+
+`
+**/
+
+btnSucess.addEventListener('click', () => {
+    document.body.innerHTML = alertSucess
+})
