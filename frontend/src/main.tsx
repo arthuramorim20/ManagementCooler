da2c033352f49039cf89c4e417c3250a3113a315
Arthur <aer47186@gmail.com>
2025-04-23 15:34:34 -0400

    ++

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,13 @@ import { Provider } from './components/ui/provider.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Demo from './App.tsx'
-import { Input } from "@chakra-ui/react"
+import MyApp from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <Demo />
-      <Input />
+      <MyApp { }/>
     </Provider>
   </StrictMode>,
 )
