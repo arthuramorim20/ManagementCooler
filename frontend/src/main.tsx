3c327d8ad24b610b2440a32932ead2d056e097fe
Arthur <aer47186@gmail.com>
2025-04-23 16:03:47 -0400

    ++

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { Provider } from './components/ui/provider.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MyApp from './App.tsx'
+import MyApp from './app/App.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
