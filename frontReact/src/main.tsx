f44781f634d04ee87d64e16324c0efec9752bd72
Arthur Evaristo <aer47186@gmail.com>
2025-05-03 21:27:03 -0400

    ++

--- frontReact/src/main.tsx
+++ frontReact/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme, ThemePanel } from '@radix-ui/themes'
 import './index.css'
-import App from './App.tsx'
+import "@radix-ui/themes/styles.css";
+import App from '../src/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <ThemePanel />
+    </Theme>
+
   </StrictMode>,
 )
