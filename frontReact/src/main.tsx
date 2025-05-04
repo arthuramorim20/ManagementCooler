c3da12982f1b6923a73dee9f7cf9a2f591c4f52d
Arthur Evaristo <aer47186@gmail.com>
2025-05-03 21:30:24 -0400

    Especify

--- frontReact/src/main.tsx
+++ frontReact/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Theme, ThemePanel } from '@radix-ui/themes'
 import './index.css'
 import "@radix-ui/themes/styles.css";
-import App from '../src/app/App.tsx'
+//import App from '../src/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
