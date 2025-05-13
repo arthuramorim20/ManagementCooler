20fde48ba00533029c2bb8184c1cdfd0e70c8c5a
Arthur Evaristo <123783284+arthuramorim20@users.noreply.github.com>
2025-05-13 01:18:08 -0300

    Delete frontReact directory

--- frontReact/src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hmetgwstcrosmapoxvvi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtZXRnd3N0Y3Jvc21hcG94dnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4ODc1NTYsImV4cCI6MjA2MjQ2MzU1Nn0.gEsAqEwpnXW8inzuB3WjhOMPqNjrrIUUd_wBMS8zp34'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
